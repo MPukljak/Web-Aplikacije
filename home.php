18665cd29f23b3af79a1fc1b8456c5879639bbdf
MPukljak <32621106+MPukljak@users.noreply.github.com>
2018-01-20 22:09:55 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,17 @@
+<?php 
+	print '
+	<h1>Kimi-Matias Räikkönen</h1>
+	<figure>
+		<img src="img/kimi.jpg" alt="Kimi" title="Kimi">
+		<figcaption>Kimi-Matias Räikkönen [ˈkimi ˈmɑtiɑs ˈræikkønen] (Espoo, Finska, 17. listopada 1979. - ), finski vozač Formule 1, svjetski prvak iz 2007. Od 2010. do 2012. se natjecao u svjetskom prvenstvu u reliju (WRC), nakon čega je objavio da se vraća u Formulu 1 kao vozač momčadi Lotusa.</figure>
+	<p>Godine 2001. ušao u Formulu 1 s timom Sauber-Petronas. Od 2002. natjecao se u timu McLaren-Mercedes, gdje je zamijenio kolegu Finca Miku Häkkinena. U prvenstvima 2003. i 2005. godine završava kao drugi, iza Michaela Schumachera i Fernanda Alonsa.
+
+Prelaskom u momčad Ferrari 2007. godine Räikkönen je postao najviše plaćeni vozač Formule [1], a iste sezone osvaja svoj prvi naslov svjetskog prvaka, sa samo jednim bodom prednosti ispred Fernanda Alonsa i Lewisa Hamiltona u jednoj od najneizvjesnijih sezona Formule 1. Timski kolega mu je bio Brazilac Felipe Massa.
+
+20 minuta prije svoje prve F1 Velike nagrade, Räikkönen je bio u krevetu i spavao. Zahvaljuljući tome što je smiren, hladan, i proračunat, dobio je nadimak "Iceman".
+	<p>Source: <a href="https://hr.wikipedia.org/wiki/Kimi_R%C3%A4ikk%C3%B6nen" target="_blank">Wiki</a></p>
+	<p>Social media:<br>
+		<a href="https://www.facebook.com/KimiRaikkonenScuderiaFerrariFans/" target="_blank"><img src="img/fb_icon.svg" alt="FB" title="Kimi" style="width:24px; margin-top:0.4em"></a>
+		<a href="https://twitter.com/kimi7iceman" target="_blank"><img src="img/twitter.svg" alt="Twitter" title="Twitter" style="width:24px; margin-top:0.4em"></a>
+		</p>';
+?>
---
+++ news.php
@@ -0,0 +1,31 @@
+<?php 
+	print '
+	<h1>NEWS</h1>
+	<div class="news">
+		<img src="news/f1_logo.jpg" alt="F1 logo plagijat" title="F1 logo plagijat">
+		<h2>F1 logo plagijat</h2>
+		<p>Formula 1 se nalazi u problemima sa zaštitnim znakom nakon što je objavljen novi logotip.
+
+Nakon što je Liberty Media krajem 2017. sezone objavila kako se zamjenjuje prepoznatljivi “leteći F” logotip koji je još iz ere Bernieja Ecclestonea, veliki broj navijača ostao je zaprepašten odlukom novih vlasnika Formule 1.
+
+No, prema pisanju novinara Christiana Sytla, novim logom nije zadovoljna i stacionirana tvrtka 3M.
+
+U novinama Telegraph napisao je kako novi logo F1 “nosi očiglednu sličnost” s već postojećim “F” logotipom koji je već registriran od strane tvrtke 3M za njihov brand.
+
+Također su se i iz tvrtke komentirali novonastalu situaciji.
+
+    “Nismo imali nikakav razgovor s drugom stranom o logotipu. Nadamo se rješavanju ovog problema u bliskoj budućnosti.” <a href="#">More ...</a></p>
+		<time datetime="2018-1-10">10 January 2018</time>
+		<hr>
+		<img src="news/Vozači.jpg" alt="Min težina" title="Min težina">
+		<h2>Uvođenje novih pravila o minimalnoj težini vozača</h2>
+		<p>Tijekom jučerašnjeg sastanka u Londonu, F1 komisija usvojila je prijedlog o uvođenju minimalne tjelesne mase vozača, a prijedlog će stupiti na snagu 2019. godine. Naime, ovo je bila tek jedna od točaka Formula One Strategy Groupa. Prijedlog nije dobio potporu barem deset članova grupe, pa su stoga promjene odgođene do sljedeće sezone.
+
+Minimalna granica će biti postavljena na 80 kilograma. Vozači s manjom težinom će morati “nositi” dodatnu težinu, odnosno balast, u svojim bolidima. Novo pravilo će omogućiti odvojenu primjenu propisa o težini za vozače i bolide. Težina vozača nije bila uključena u ukupnu težinu bolida do 1995. godine, kada je težina povećana za 80 kilograma, na ukupno 595 kilograma uključujući vozača. Navedena promjena je neophodna zbog uvođenja kontroverzne “halo” zaštite zbog koje je dodatno povećana težina bolida, uslijed čega su viši vozači u još nepovoljnijem položaju. Za usporedbu, prošle godine je najniži vozač bio Felipe Massa (1,66 metara), a najviši Esteban Ocon (1,86 metara), što je činilo razliku od čak petnaest kilograma.
+
+Strategy Group je raspravljala i o daljnjim promjenama veličine i položaja identifikacijskih brojeva i brojeva vozača, kao i promjenama u rasporedu trkaćeg vikenda, no nisu doneseni nikakvi zaključci za 2018. godinu. <a href="#">More ...</a></p>
+		<time datetime="2018-1-10">10 January 2018</time>
+		<hr>
+		
+	</div>';
+?>
